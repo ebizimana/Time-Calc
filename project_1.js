c605f65b878ec0098d50cfcb599b512d6c16e81e
cleptest <cleptest@liberty.edu>
2018-12-04 14:54:04 -0500

    Changed the design and fix the time bug

--- project_1.js
+++ project_1.js
@@ -1,54 +1,55 @@
 var el = document.getElementById('time_span')
+var input = document.getElementById("minIn");
+
 setInterval(function() {
+  var currentTime = new Date(),
+    hours = currentTime.getHours(),
+    minutes = currentTime.getMinutes(),
+    ampm = hours > 11 ? 'PM' : 'AM';
 
-    var currentTime = new Date(),
-        hours = currentTime.getHours(),
-        minutes = currentTime.getMinutes(),
-        ampm = hours > 11 ? 'PM' : 'AM';
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (hours > 12) {
+    hours -= 12
+  }
 
-     if (hours <10)
-     {
-       hours = "0" + hours;
-      }
-
-    if (minutes <10)
-     {
-       minutes = "0" + minutes;
-      }
-
-    el.innerHTML = hours + ":" + minutes + " " + ampm;
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  el.innerHTML = hours + ":" + minutes + " " + ampm;
 }, 1000);
 
-function time_limite(){
-    var currentTime = new Date();
-    var hourIn = document.getElementById("hourIn").value;
-    var minIn = document.getElementById("minIn").value;
-    var resulth = document.getElementById("resulth");
+// To make sure the "Enter Key" Works
+input.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("myBtn").click();
+  }
+});
 
-var result = new Date(currentTime.getTime() + (hourIn *3600000)+ (minIn *60*1000));
+function time_limite() {
+  var currentTime = new Date();
+  var hourIn = document.getElementById("hourIn").value;
+  var minIn = document.getElementById("minIn").value;
+  var resulth = document.getElementById("resulth");
 
-    var newhour = result.getHours();
-    var newmin = result.getMinutes();
-    var ampm = newhour > 11 ? 'PM' : 'AM';
-    if (newmin <10)
-     {
-       newmin = "0" + newmin;
-      }
+  var result = new Date(currentTime.getTime() + (hourIn * 3600000) + (minIn * 60 * 1000));
 
-    if (newhour <= 12)
-        {
-            resulth.innerHTML = ("Hour: " + newhour + ":" + newmin + " "+ ampm);
-        }
+  var newhour = result.getHours();
+  var newmin = result.getMinutes();
+  var ampm = newhour > 11 ? 'PM' : 'AM';
+  if (newmin < 10) {
+    newmin = "0" + newmin;
+  }
 
-    else if (newhour >= 12 && newhour < 17)
-    {
-            var hourn = newhour - 12;
-            resulth.innerHTML = ("Hour: " + hourn + ":" + newmin + " " + ampm);
-        }
-
-    else if (newhour >= 17)
-    {
-            resulth.innerHTML =("5:00 WE ARE CLOSED");
-        }
+  if (newhour <= 12) {
+    resulth.innerHTML = ("Hour: " + newhour + ":" + newmin + " " + ampm);
+  } else if (newhour > 12 && newhour < 17) {
+    var hourn = newhour - 12;
+    resulth.innerHTML = ("Hour: " + hourn + ":" + newmin + " " + ampm);
+  } else if (newhour >= 17) {
+    resulth.innerHTML = ("5:00 WE ARE CLOSED");
+  }
 
 }
